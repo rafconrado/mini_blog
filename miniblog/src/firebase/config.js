7c6473c1d548c753c3f8db43cc104220e3e670c5
Rafael Conrado de Souza <rafconradoo@gmail.com>
2024-04-30 14:34:30 -0300

    miniblog first

---
+++ miniblog/src/firebase/config.js
@@ -0,0 +1,17 @@
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firebase";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAhXVNKhgNhZX6iMZVhJnu8_Q8Qzedhoy8",
+  authDomain: "miniblog-5d65e.firebaseapp.com",
+  projectId: "miniblog-5d65e",
+  storageBucket: "miniblog-5d65e.appspot.com",
+  messagingSenderId: "593391902146",
+  appId: "1:593391902146:web:ede28b8be3cd5f4e2f4a5b",
+};
+
+const app = initializeApp(firebaseConfig);
+
+const db = getFirestore(app);
+
+export { db };
