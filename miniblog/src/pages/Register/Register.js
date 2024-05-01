892655f1cd6a88334e77354b7918b3f7f384ecb4
Rafael Conrado de Souza <rafconradoo@gmail.com>
2024-05-01 00:18:28 -0300

    commit

--- miniblog/src/pages/Register/Register.js
+++ miniblog/src/pages/Register/Register.js
@@ -6,10 +6,45 @@ const Register = () => {
   return (
     <div>
       <h1>Cadastre-se para postar</h1>
+      <p>Crie o seu usuário e compartilhe suas histórias!</p>
       <form>
         <label>
-          <span></span>
+          <span>Nome:</span>
+          <input 
+          type="text"
+          name="displayName"
+          required
+          placeholder="Nome do usuário"
+          />
         </label>
+        <label>
+          <span>E-mail:</span>
+          <input 
+          type="email"
+          name="email"
+          required
+          placeholder="E-mail do usuário"
+          />
+        </label>
+        <label>
+          <span>Senha:</span>
+          <input 
+          type="password"
+          name="password"
+          required
+          placeholder="Insira sua senha"
+          />
+        </label>
+        <label>
+          <span>Confirmação de senha:</span>
+          <input 
+          type="password"
+          name="confirmPassword"
+          required
+          placeholder="Confirme a sua senha"
+          />
+        </label>
+        <button class="btn">Cadastrar</button>
       </form>
     </div>
   );
