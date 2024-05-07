6b1a513ab13df1684a5ff253b116cd8561c53191
Rafael Conrado de Souza <rafconradoo@gmail.com>
2024-05-07 00:13:57 -0300

    Update Dashboard.js

--- miniblog/src/pages/Dashboard/Dashboard.js
+++ miniblog/src/pages/Dashboard/Dashboard.js
@@ -1,9 +1,34 @@
 import styles from "./Dashboard.module.css";
 
+import { Link } from "react-router-dom";
+
+//HOOKS
+import { useAuthValue } from "../../context/AuthContext";
+//import {useFetchDocuments} from "../../hooks/useFetchDocumets"
+
 const Dashboard = () => {
+  const { user } = useAuthValue();
+  const uid = user.uid;
+
+  //POSTS FROM USER
+  const posts = [];
+
   return (
     <div>
-      <h1>Dashboard</h1>
+      <h2>Dashboard</h2>
+      <p>Gerencie os seus posts</p>
+      {posts && posts.length === 0 ? (
+        <div className={styles.noposts}>
+          <p>Não foram encontrados posts.</p>
+          <Link to="./posts/create" className="btn">
+            Criar primeiro post
+          </Link>
+        </div>
+      ) : (
+        <div>
+          <p>Tem posts!</p>
+        </div>
+      )}
     </div>
   );
 };
