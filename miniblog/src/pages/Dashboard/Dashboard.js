b7d9bf0ae04001a1aa2ffb24cfefbd94a8b22f5f
Rafael Conrado de Souza <rafconradoo@gmail.com>
2024-05-19 13:08:45 -0300

    att

--- miniblog/src/pages/Dashboard/Dashboard.js
+++ miniblog/src/pages/Dashboard/Dashboard.js
@@ -4,17 +4,23 @@ import { Link } from "react-router-dom";
 
 //HOOKS
 import { useAuthValue } from "../../context/AuthContext";
-//import {useFetchDocuments} from "../../hooks/useFetchDocumets"
+import { useFetchDocuments } from "../../hooks/useFetchDocuments";
+import { useDeleteDocument } from "../../hooks/useDeleteDocuments";
 
 const Dashboard = () => {
   const { user } = useAuthValue();
   const uid = user.uid;
 
-  //POSTS FROM USER
-  const posts = [];
+  const { documents: posts, loading } = useFetchDocuments("posts", null, uid);
+
+  const { deleteDocument } = useDeleteDocument("posts");
+
+  if (loading) {
+    return <p>Carregando...</p>;
+  }
 
   return (
-    <div>
+    <div className={styles.dashboard}>
       <h2>Dashboard</h2>
       <p>Gerencie os seus posts</p>
       {posts && posts.length === 0 ? (
@@ -25,9 +31,36 @@ const Dashboard = () => {
           </Link>
         </div>
       ) : (
-        <div>
-          <p>Tem posts!</p>
-        </div>
+        <>
+          <div className={styles.post_header}>
+            <span>Título</span>
+            <span>Ações</span>
+          </div>
+
+          {posts &&
+            posts.map((post) => (
+              <div key={post.id} className={styles.post_row}>
+                <p>{post.title}</p>
+                <div>
+                  <Link to={`/posts/${post.id}`} className="btn btn-outline">
+                    Ver
+                  </Link>
+                  <Link
+                    to={`/posts/edit/${post.id}`}
+                    className="btn btn-outline"
+                  >
+                    Editar
+                  </Link>
+                  <button
+                    onClick={() => deleteDocument(post.id)}
+                    className="btn btn-outline btn-danger"
+                  >
+                    Excluir
+                  </button>
+                </div>
+              </div>
+            ))}
+        </>
       )}
     </div>
   );
